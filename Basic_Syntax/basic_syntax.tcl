8b5c45a2cfe05e3ce6d5187ac5bb1017e73ff5a7
Ganesh <ganesheart1@gmail.com>
2025-07-14 19:22:37 +0530

    Create basic_syntax.tcl

---
+++ Basic_Syntax/basic_syntax.tcl
@@ -0,0 +1,26 @@
+#First Program
+
+puts "Hello World!"
+
+
+#Comments in TCL
+
+#For single-line comments
+
+#for multiple lines we can use "if 0 {comments}"
+
+if 0 {
+
+This is for the 
+Multiple-line comments
+
+}
+
+puts "Hello World"
+
+
+#whitespace in TCL
+
+puts "Hello World!"
+
+puts [expr 1+6+9]
