a376859c4c5739e0127b69073cda5483ddc62b43
Ganesh <ganesheart1@gmail.com>
2025-07-19 16:28:31 +0530

    Create String_Representation.tcl

---
+++ Data_Types/String_Representation.tcl
@@ -0,0 +1,24 @@
+#String Representation
+
+if 0 {
+
+Unlike other languages, in Tcl, you need not include double quotes when it's only a single
+word. An example can be
+}
+
+set myvariable Hello
+
+puts $myvariable
+
+if 0 {
+
+When we want to represent multiple strings, we can use either double quotes or curly
+braces. It is shown below
+
+}
+
+set myvariable1 "Hello world"
+puts $myvariable1
+
+set myvariable2 {Hello World}
+puts $myvariable2
