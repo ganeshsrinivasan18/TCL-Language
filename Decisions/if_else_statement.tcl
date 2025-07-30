ea2c26b9150304ffb2031afd47c78e1d2f34f205
Ganesh <ganesheart1@gmail.com>
2025-07-30 19:48:37 +0530

    If - else Statement in TCL

---
+++ Decisions/if_else_statement.tcl
@@ -0,0 +1,15 @@
+#if-else statement in TCL
+
+set a 20
+
+if { $a < 10 } {
+
+puts "The value of a is less than 10\n"
+
+} else {
+
+puts "The value of a is greater than 10\n"
+
+}
+
+puts "The exact value of a is $a"
