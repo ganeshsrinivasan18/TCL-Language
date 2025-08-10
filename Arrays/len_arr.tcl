e5c58abfc27cd910f0599f95618510a0d488e1d5
Ganesh <ganesheart1@gmail.com>
2025-08-10 12:35:17 +0530

    Length of an Array in TCL

---
+++ Arrays/len_arr.tcl
@@ -0,0 +1,14 @@
+#Length of an Array element in TCL
+
+set arr(0) Ganesh
+
+set arr(1) Saranya
+
+puts "The length of the Array elements in TCL"
+
+puts [string length $arr(0)]
+
+puts [string length $arr(1)]
+
+
+
