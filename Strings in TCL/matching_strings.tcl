a1e556d8bbb674126a1a7b6e47b2a158e23552d8
Ganesh <ganesheart1@gmail.com>
2025-10-05 17:55:57 +0530

    matching_strings in TCL

---
+++ Strings in TCL/matching_strings.tcl
@@ -0,0 +1,13 @@
+#Matching_Strings in TCL
+
+set s1 "test@test.com"
+
+set s2 "*@*.com"
+
+puts "Matching pattern s2 in s1"
+
+puts [string match "*@*.com" $s1 ]
+
+puts "Matching pattern tcl in s1"
+
+puts [string match {tcl} $s1]
